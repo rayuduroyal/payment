6197332acdd5f5e1c5a4c60c44f5a638336af4b5
susarlarayudu <rayuduroyal1996@gmail.com>
2022-01-24 10:48:26 +0530

    auto pushing chainges repo

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+@Library('roboshop') _
+
+// python(
+//     COMPONENT         : 'payment',
+//     LABEL             : 'WORKSTATION'
+// )
+
+python-immutable(
+    COMPONENT         : 'payment',
+    LABEL             : 'WORKSTATION'
+)
---
+++ terraform-mutable/main.tf
@@ -0,0 +1,18 @@
+module "payment" {
+  source                 = "git::https://github.com/rayuduroyal/roboshop.git/terraform-mutable//app-module"
+  INSTANCE_TYPE          = var.INSTANCE_TYPE
+  ENV                    = var.ENV
+  SPOT_INSTANCE_COUNT    = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_COUNT      = var.OD_INSTANCE_COUNT
+  COMPONENT              = "payment"
+  PORT                   = 8080
+  IS_PRIVATE_IB          = true
+  LB_RULE_PRIORITY       = 103
+  APP_VERSION            = var.APP_VERSION
+  NEXUS_USER             = var.NEXUS_USER
+  NEXUS_PASS             = var.NEXUS_PASS
+}
+
+output "INSTANCE_IDS" {
+  value = module.payment.INSTANCE_IDS
+}
